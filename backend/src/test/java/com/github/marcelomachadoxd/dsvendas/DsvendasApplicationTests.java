066a763bc53acebb33ba9016cfb59dd4d0f7471a
MarceloMachadoxD <mastermanson@gmail.com>
2021-05-03 19:48:57 -0300

    Project created

---
+++ backend/src/test/java/com/github/marcelomachadoxd/dsvendas/DsvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.marcelomachadoxd.dsvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

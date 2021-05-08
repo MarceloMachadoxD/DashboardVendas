fc566e51ce5ff4377a528185e96abfc8fd937115
MarceloMachadoxD <mastermanson@gmail.com>
2021-05-08 11:42:42 -0300

    Datatable integration

--- frontend/src/components/DataTable/index.tsx
+++ frontend/src/components/DataTable/index.tsx
@@ -1,6 +1,35 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { SalePage } from 'types/sale';
+import { formatLocalDate } from 'utils/format';
+import { BASE_URL } from 'utils/requests';
 
 const DataTable = () => {
+
+
+    const [Page, setPage] = useState<SalePage>({
+        first: true,
+        last: true,
+        number: 0,
+        totalElements: 0,
+        totalPages: 0
+    });
+
+
+    useEffect(() => {
+
+        axios.get(`${BASE_URL}sales?page=0&size=30&sort=date,desc`)
+            .then(response => {
+                setPage(response.data)
+            })
+
+
+
+    }, [])
+
+
+
+
     return (
         <div className="table-responsive">
             <table className="table table-striped table-sm">
@@ -14,13 +43,17 @@ const DataTable = () => {
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td>22/04/2021</td>
-                        <td>Barry Allen</td>
-                        <td>34</td>
-                        <td>25</td>
-                        <td>15017.00</td>
-                    </tr>
+                    {Page.content?.map(item => (
+
+                        <tr key={item.id}>
+                            <td>{formatLocalDate(item.date, "dd/MM/yy")}</td>
+                            <td>{item.seller.name}</td>
+                            <td>{item.visited}</td>
+                            <td>{item.deals}</td>
+                            <td>{item.amount.toFixed(2)}</td>
+                        </tr>
+                    ))}
+                    
                 </tbody>
             </table>
         </div>
